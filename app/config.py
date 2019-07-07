9e616f93385189fd3fc8b17cf0ab99fdd19fad5e
zhumabek <jumabekzalkarov@gmail.com>
2019-07-07 19:12:59 +0600

    my second comnit()

--- app/config.py
+++ app/config.py
@@ -1,6 +1,10 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__name__))
+
 
 class Config(object):
     SECRET_KEY = 'admin'
-    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
